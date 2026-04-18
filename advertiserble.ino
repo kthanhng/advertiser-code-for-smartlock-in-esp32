b20c3b131d9ee1ee9fb4b9d8fe9f86db2f7d6396
kthanhng <thanhkhoanguyen01082004@gmail.com>
2026-04-18 23:18:48 +0700

    code advertiser nhung cho multiple access ko gioi han thiet bi truy cap

--- advertiserble.ino
+++ advertiserble.ino
@@ -1,62 +1,60 @@
+// code cho mutiple access còn đâu như code trên 
 #include <Arduino.h>
 #include <BLEDevice.h>
 #include <BLEServer.h>
 #include <BLEUtils.h>
 #include <BLEAdvertising.h>
 
-
 #define SERVICE_UUID "12345678-1234-1234-1234-1234567890ab"
-
 
 BLEServer *pServer = NULL;
 
+class MyServerCallbacks : public BLEServerCallbacks {
+    void onConnect(BLEServer* pServer) {
+        Serial.println("Client connected");
+        // Tiếp tục phát advertising để người khác vẫn thấy và connect được
+        delay(100);
+        BLEDevice::startAdvertising();
+    }
+
+    void onDisconnect(BLEServer* pServer) {
+        Serial.println("Client disconnected -> Restart advertising");
+        delay(500);
+        BLEDevice::startAdvertising();
+    }
+};
 
 void setup() {
-  Serial.begin(115200);
+    Serial.begin(115200);
 
+    BLEDevice::init("ESP32_Lock");
 
-  // Khởi tạo tên(Tên này sẽ được nhét vào gói phụ)
-  BLEDevice::init("ESP32_Lock");
+    pServer = BLEDevice::createServer();
+    pServer->setCallbacks(new MyServerCallbacks());
 
+    BLEService *pService = pServer->createService(SERVICE_UUID);
+    pService->start();
 
-  // Xây GATT Server để Android connect không bị lỗi 133 / 19
-  pServer = BLEDevice::createServer();
-  BLEService *pService = pServer->createService(SERVICE_UUID);
-  pService->start();
+    BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
 
+    BLEAdvertisementData advData;
+    advData.setFlags(0x06);
+    advData.setCompleteServices(BLEUUID(SERVICE_UUID));
+    pAdvertising->setAdvertisementData(advData);
 
-  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+    BLEAdvertisementData scanResponseData;
+    scanResponseData.setName("ESP32_Lock");
+    pAdvertising->setScanResponseData(scanResponseData);
 
+    pAdvertising->setScanResponse(true);
+    pAdvertising->setMinInterval(0x20);
+    pAdvertising->setMaxInterval(0x40);
 
-  // 👇 1. GÓI CHÍNH (Chỉ nhét UUID, dung lượng 21/31 bytes -> Rất an toàn)
-  BLEAdvertisementData advData;
-  advData.setFlags(0x06);
-  advData.setCompleteServices(BLEUUID(SERVICE_UUID));
-  pAdvertising->setAdvertisementData(advData);
+    BLEDevice::startAdvertising();
 
-
-  // 👇 2. GÓI PHỤ (Nhét tên thiết bị vào đây)
-  BLEAdvertisementData scanResponseData;
-  scanResponseData.setName("ESP32_Lock");
-  pAdvertising->setScanResponseData(scanResponseData);
-
-
-  // Kích hoạt cho phép ESP32 ném gói phụ ra khi bị Android hỏi
-  pAdvertising->setScanResponse(true);
-
-
-  // Ép xung tốc độ phát (20ms - 40ms)
-  pAdvertising->setMinInterval(0x20);
-  pAdvertising->setMaxInterval(0x40);
-
-
-  BLEDevice::startAdvertising();
-
-
-  Serial.println("Đã fix lỗi 31-byte! Gói tin chuẩn đang được phát......");
+    Serial.println("BLE Server ready - Multi-connect enabled");
 }
 
-
 void loop() {
-  // Để trống
+    // Để trống
 }
